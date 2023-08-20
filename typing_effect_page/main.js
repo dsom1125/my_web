49a5b2684461c46f755920a919a830cb4e51dc2f
dsom1125 <96814848+dsom1125@users.noreply.github.com>
2023-08-20 12:41:43 +0900

    Update main.js

--- typing_effect_page/main.js
+++ typing_effect_page/main.js
@@ -1,6 +1,14 @@
 // main.js
 document.addEventListener("DOMContentLoaded", () => {
     const icon = document.getElementById("icon");
+    const dynamicText = document.getElementById("dynamic");
+
+    function toggleCursor() {
+        dynamicText.classList.toggle("active");
+    }
+
+    setInterval(toggleCursor, 500); // 0.5초마다 커서 깜박임 효과
+
     icon.addEventListener("click", () => {
         icon.classList.toggle("active");
     });
